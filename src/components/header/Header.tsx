81160a9a238df557b35523a250ca988a597bbb20
toshadrugdealer <t.reikh@yandex.ru>
2024-08-12 22:22:49 +0300

    add modal

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,31 @@
 import { BsBookmarkHeart } from "react-icons/bs";
 import styles from "./Header.module.css";
 import { useFavorites } from "../../hooks/useFavorites";
+import { useState } from "react";
 
 export function Header() {
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => {
+    setShowModal(true);
+  };
   const { favorites } = useFavorites();
+  console.log(favorites);
   return (
     <header className={styles.header}>
-      <BsBookmarkHeart fontSize={40} />
-      <span>{favorites.length}</span>
+      {showModal && (
+        <div className={styles.modal}>
+          <p>
+            {favorites.length === 0
+              ? "Favorites is empty"
+              : favorites.map((item) => item.name).join(", ")}
+          </p>
+          <button onClick={() => setShowModal(false)}>Close</button>
+        </div>
+      )}
+      <div onClick={openModal}>
+        <BsBookmarkHeart fontSize={40} />
+        <span className={styles.span}>{favorites.length}</span>
+      </div>
     </header>
   );
 }
